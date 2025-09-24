9a4a4019113ebbc87b99cd940a62d16d558e9bd6
Edison Wang <ew040607@gmail.com>
2025-09-23 20:17:46 -0400

    created poll views and added them to urls.py

--- pollboard/polls/urls.py
+++ pollboard/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import PollListView, PollSingleView
 
 urlpatterns = [
-    
+    path("polls/", PollListView.as_view(), name="poll-list"),
+    path("polls/<uuid:poll_id>", PollSingleView.as_view(), name="poll-single")
 ]
--- pollboard/polls/views.py
+++ pollboard/polls/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PollSerializer, DraftSerializer, UserSerializer, OptionSerializer
+from .models import Poll, Draft, User, Option
 
 # Create your views here.
+
+class PollListView(generics.ListAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class PollSingleView(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    lookup_field = "poll_id"
+
+    def destroy(self, request, *args, **kwargs):
+        poll = self.get_object()
+        user = request.user
+        if not (user.is_superuser or poll.user_id == user):
+            raise PermissionDenied("You do not have permission to delete this poll.")
+        Option.objects.filter(poll_id=poll).delete()
+        poll.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
