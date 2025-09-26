49f74672b2fe130ccaa305d840867d35f4f8b254
Edison Wang <ew040607@gmail.com>
2025-09-26 15:25:11 -0400

    added endpoints to views

--- pollboard/polls/urls.py
+++ pollboard/polls/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import PollListView, PollSingleView
+from .views import PollListView, PollSingleView, PollCreateView, DraftListView, DraftSingleView, PollLikeView, VoteView
 
 urlpatterns = [
     path("polls/", PollListView.as_view(), name="poll-list"),
-    path("polls/<uuid:poll_id>", PollSingleView.as_view(), name="poll-single")
+    path("polls/<uuid:poll_id>", PollSingleView.as_view(), name="poll-single"),
+    path("polls/post/<uuid:draft_id>", PollCreateView.as_view(), name="poll-create"),
+    path("drafts/", DraftListView.as_view(), name="draft-list"),
+    path("drafts/<uuid:draft_id>", DraftSingleView.as_view(), name="draft-single"),
+    path("polls/like/<uuid:poll_id>", PollLikeView.as_view(), name="poll-like"),
+    path("polls/vote/<uuid:option_id>", VoteView.as_view(), name="poll-vote"),
 ]
