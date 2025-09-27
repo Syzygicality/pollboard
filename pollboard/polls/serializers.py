a1d4325d3b9d6e204feeeb7146bdd816b8b6cbd8
Edison Wang <ew040607@gmail.com>
2025-09-27 19:42:50 -0400

    general fixes and added id fields to serializers

--- pollboard/polls/serializers.py
+++ pollboard/polls/serializers.py
@@ -4,26 +4,27 @@ from .models import User, Poll, Option, Draft
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username"]
+        fields = ["user_id", "username"]
 
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
-        fields = ["choice", "votes"]
+        fields = ["option_id", "choice", "votes"]
 
 class PollSerializer(serializers.ModelSerializer):
     options = serializers.SerializerMethodField()
-    user_id = UserSerializer()
 
     class Meta:
         model = Poll
         fields = ["user_id", "title", "description", "options", "likes"]
     
     def get_options(self, obj):
-        options = Option.objects.filter(id__in=obj.options)
+        option_ids = obj.options
+        options_map = {o.option_id: o for o in Option.objects.filter(option_id__in=option_ids)}
+        options = [options_map[option_id] for option_id in option_ids if option_id in options_map]
         return OptionSerializer(options, many=True).data
     
 class DraftSerializer(serializers.ModelSerializer):
     class Meta:
         model = Draft
-        fields = ["title", "description", "options"]
+        fields = ["draft_id", "title", "description", "options"]
