53149f934b2fa05d49fa9191d882ab1d0f04e709
Edison Wang <ew040607@gmail.com>
2025-12-20 11:55:39 -0700

    setup apps

--- app/pollboard/urls.py
+++ app/pollboard/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+    path("/", include("polls.urls")),
+    path("/", include("users.urls")),
 ]
