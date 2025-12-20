ac6fab0d491dc8bee3391bafabf55628fb44d1f0
Edison Wang <ew040607@gmail.com>
2025-12-20 15:17:55 -0700

    users app urls

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,5 +1,7 @@
-from .views import *
+from .views import CharacteristicsCreateView, CharacteristicsSingleView
+from django.urls import path
 
 urlpatterns = [
-    
+    path('characteristics', CharacteristicsSingleView.as_view(), name='get_characteristics'),
+    path('characteristics/post', CharacteristicsCreateView.as_view(), name='create_characteristics'),
 ]
