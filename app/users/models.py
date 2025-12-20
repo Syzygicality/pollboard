6dafdbd130297d85e4537e1fea4682aec678cbd6
Edison Wang <ew040607@gmail.com>
2025-12-20 13:21:32 -0700

    create user characteristics model

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django_countries.fields import CountryField
+import shortuuid
 
-# Create your models here.
+User = get_user_model()
+
+class Characteristics(models.Model):
+    """
+    Stores demographic traits for a user. 
+    Age is stored as a bucket to reduce re-identification risk.
+    """
+    id = models.CharField(primary_key=True, default=shortuuid.uuid, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="characteristics")
+    country = CountryField(blank=True, null=True)
+    gender = models.CharField(max_length=32, default="Prefer not to say", blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Characteristics for {self.user.username}"
