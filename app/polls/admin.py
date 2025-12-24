97930b4952dc09a816fe028537ecc4ed412c94ac
Edison Wang <ew040607@gmail.com>
2025-12-23 20:00:28 -0700

    register polls app models

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -1,3 +1,9 @@
+from .models import Category, Option, Poll, Like
+
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Option)
+admin.site.register(Poll)
+admin.site.register(Like)
